1fc224221833638d7bcac165ec48af3dc2b4e362
Forgman75 <agolikov2020@gmail.com>
2026-03-07 15:39:01 +0300

    Added the get_date function

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from src.widget import mask_account_card
+from src.widget import get_date, mask_account_card
+
 
 def main():
+    DATE_STR_ISO = "2024-03-11T02:26:18.671407"
+
     list_of_payment = [
         "Maestro 1596837868705199",
         "Счет 64686473678894779589",
@@ -9,11 +12,13 @@ def main():
         "Visa Classic 6831982476737658",
         "Visa Platinum 8990922113665229",
         "Visa Gold 5999414228426353",
-        "Счет 73654108430135874305"
+        "Счет 73654108430135874305",
     ]
     for i in range(len(list_of_payment)):
         print(mask_account_card(list_of_payment[i]))
 
+    print(get_date(DATE_STR_ISO))
+
+
 if __name__ == "__main__":
     main()
-
--- src/widget.py
+++ src/widget.py
@@ -1,25 +1,40 @@
-from src.masks import get_mask_card_number, get_mask_account
+from datetime import datetime
+
+from src.masks import get_mask_account, get_mask_card_number
+
 
 def mask_account_card(info: str) -> str:
-    """Функция для маскировки карт и счетов.
+    """
+    Функция для маскировки карт и счетов.
     Она разделяет строку на части по пробелу.
     Получается две части: тип карты или счет, и номер карты или счета.
     По длине номера определяем, карта это или счет. Затем вызываем соответствующую
     функцию маскирования, соединяем результат с типом и возвращаем пользователю.
 
     """
-    
+
     parts = info.split()
-    
-    card_or_account_type = " ".join(parts[:-1])  
-    number = parts[-1]  
-    
+
+    card_or_account_type = " ".join(parts[:-1])
+    number = parts[-1]
+
     if len(number) == 16:
         masked_number = get_mask_card_number(int(number))
     elif len(number) == 20:
         masked_number = get_mask_account(int(number))
     else:
         raise ValueError("Некорректный номер карты или счета.")
-    
+
     return f"{card_or_account_type} {masked_number}"
 
+
+def get_date(date: str) -> str:
+    """
+    Функция для преобразования строки с датой в формате ISO 8601
+    в строку вида "ДД.ММ.ГГГГ"
+    """
+
+    datetime_from_iso = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%f")
+    result_date = datetime_from_iso.strftime("%d.%m.%Y")
+
+    return result_date
