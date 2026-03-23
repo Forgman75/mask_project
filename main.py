2235b85dd766cd8a02c82622ddd65822b1334e2c
Forgman75 <agolikov2020@gmail.com>
2026-03-23 20:23:35 +0300

    Fixed the main

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+from src.processing import filter_by_state, sort_by_date
 from src.widget import get_date, mask_account_card
 
 
 def main():
     DATE_STR_ISO = "2024-03-11T02:26:18.671407"
+    LIST_DICT = [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
 
     list_of_payment = [
         "Maestro 1596837868705199",
@@ -18,6 +25,8 @@ def main():
         print(mask_account_card(list_of_payment[i]))
 
     print(get_date(DATE_STR_ISO))
+    print(filter_by_state(LIST_DICT))
+    print(sort_by_date(LIST_DICT))
 
 
 if __name__ == "__main__":
