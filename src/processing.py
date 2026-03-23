942479f7353979e3b41d8ac43d3fbc6d7be9ccab
Forgman75 <agolikov2020@gmail.com>
2026-03-23 03:17:53 +0300

    Added the filter_by_state and sort_by_date function

---
+++ src/processing.py
@@ -0,0 +1,23 @@
+from operator import itemgetter
+
+def filter_by_state(input_dict: list[dict], state: str | None='EXECUTED') -> list[dict]:
+    """
+    Функция возвращает новый список словарей, содержащий только те словари, у которых ключ 
+    state
+    соответствует указанному значению.
+    """
+    return [
+         result_dict for result_dict in input_dict
+         if result_dict['state'] == state
+    ]
+
+
+
+
+def sort_by_date(input_dict: list[dict], ascending: bool=True) -> list[dict]:
+    """
+    Функция должна возвращать новый список, отсортированный по дате 
+    (date).
+    """
+
+    return sorted(input_dict, key=itemgetter("date"), reverse=ascending)
