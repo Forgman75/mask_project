29e4a66d3d112f02dc94f47513e9ec6fa7b14b90
Forgman75 <agolikov2020@gmail.com>
2026-03-23 20:28:57 +0300

    Fixed variable names and docstring

--- src/processing.py
+++ src/processing.py
@@ -1,22 +1,18 @@
 from operator import itemgetter
 
-def filter_by_state(input_dict: list[dict], state: str | None='EXECUTED') -> list[dict]:
+
+def filter_by_state(input_dict: list[dict], state: str | None = "EXECUTED") -> list[dict]:
     """
-    Функция возвращает новый список словарей, содержащий только те словари, у которых ключ 
+    Функция возвращает новый список словарей, содержащий только те словари, у которых ключ
     state
     соответствует указанному значению.
     """
-    return [
-         result_dict for result_dict in input_dict
-         if result_dict['state'] == state
-    ]
+    return [result_dict for result_dict in input_dict if result_dict["state"] == state]
 
 
-
-
-def sort_by_date(input_dict: list[dict], ascending: bool=True) -> list[dict]:
+def sort_by_date(input_dict: list[dict], ascending: bool = True) -> list[dict]:
     """
-    Функция должна возвращать новый список, отсортированный по дате 
+    Функция должна возвращать новый список, отсортированный по дате
     (date).
     """
 
