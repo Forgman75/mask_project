3453b1f488aedd3eb81a5084b89a620bdb605452
Forgman75 <agolikov2020@gmail.com>
2026-03-24 18:35:22 +0300

    Code formatting is consistent with pep8

--- src/processing.py
+++ src/processing.py
@@ -1,16 +1,23 @@
 from operator import itemgetter
 
 
-def filter_by_state(in_list_dicts: list[dict], state: str | None = "EXECUTED") -> list[dict]:
+def filter_by_state(
+    in_list_dicts: list[dict], state: str | None = "EXECUTED"
+) -> list[dict]:
     """
-    Функция возвращает новый список словарей, содержащий только те словари, у которых ключ
-    state
-    соответствует указанному значению.
+    Функция возвращает новый список словарей, содержащий только те словари,
+    у которых ключ state соответствует указанному значению.
     """
-    return [result_dict for result_dict in in_list_dicts if result_dict["state"] == state]
+    return [
+        result_dict
+        for result_dict in in_list_dicts
+        if result_dict["state"] == state
+    ]
 
 
-def sort_by_date(in_list_dicts: list[dict], reverse: bool = True) -> list[dict]:
+def sort_by_date(
+    in_list_dicts: list[dict], reverse: bool = True
+) -> list[dict]:
     """
     Функция должна возвращать новый список, отсортированный по дате
     (date). По умолчанию порядок сортировки обратный.
